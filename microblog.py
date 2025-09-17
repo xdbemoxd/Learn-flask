4c228fa5eaa1117ccc57a08be6deef8723821c1f
Benjamin <benjaminenma99@gmail.com>
2025-09-16 20:22:29 -0400

    chapter 4

--- microblog.py
+++ microblog.py
@@ -1,7 +1,8 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.model import User, Post
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
