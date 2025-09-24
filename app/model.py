20af76d69f66b31c4cb6cc4a937f60359e3f1759
Benjamin <benjaminenma99@gmail.com>
2025-09-24 11:53:32 -0400

    Chapter 4

--- app/model.py
+++ app/model.py
@@ -3,19 +3,29 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
-                                                unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
-                                             unique=True)
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+class User( UserMixin, db.Model ):
+    
+    id: so.Mapped[ int ] = so.mapped_column( primary_key = True )
+    username: so.Mapped[ str ] = so.mapped_column(sa.String( 64 ), index = True,
+                                                unique = True )
+    email: so.Mapped[ str ] = so.mapped_column( sa.String( 120 ), index = True,
+                                             unique = True )
+    password_hash: so.Mapped[ Optional[ str ] ] = so.mapped_column( sa.String( 256 ) )
 
     posts = db.relationship('Post', back_populates='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -31,3 +41,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
 from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
-from app import app
+from app.forms import LoginForm, RegistrationForm
+from app import app, db
+from app.model import User
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+from flask import request
+from urllib.parse import urlsplit
 
-@app.route("/")
-def hello_world():
-    print("hello, world!")
-    return "<p>Hello, World!</p>"
 
+@app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Benjamim Marroquin'}
     posts = [
@@ -20,13 +23,44 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts = posts)
+    return render_template("index.html", title='Home Page', posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
