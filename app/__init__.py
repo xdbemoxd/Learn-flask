2859a3d9cbfee5d13dd509f65cfd7fdf68b3b2d4
Benjamin <benjaminenma99@gmail.com>
2025-10-29 12:31:35 -0400

    chapter 10

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
-
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +15,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+
 
 if not app.debug:
     
--- app/model.py
+++ app/model.py
@@ -7,6 +7,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 from hashlib import md5
+from time import time
+import jwt
+from app import app
 
 
 followers = sa.Table(
@@ -92,6 +95,20 @@ class User( UserMixin, db.Model ):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+    
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
